6ddd9885ad1327790169741b8338d28416b37bd6
Dominic Holm-Kander <dominic.holmkander@gmail.com>
2019-11-18 11:15:01 +0000

    refactored css file and added event listener for 'get inspired' button

--- public/script.js
+++ public/script.js
@@ -1,14 +1,21 @@
-$("form").submit(event => {
-  event.preventDefault();
-  axios.post("").then(response => {
-    quote = response.data;
-    $("#text").css({
-      "text-align": "center",
-      "min-height": "150px",
-      display: "flex",
-      "justify-content": "center",
-      "align-items": "center"
+
+// Wrapped script in .ready method so that it only runs once page is adequately loaded.
+$(document).ready(() => {
+  let quoteRequested = document.querySelector("#quote-request-button");
+  quoteRequested.addEventListener("click", event => {
+    event.preventDefault();
+    console.log("click registered");
+    axios.post("").then(response => { //what's axios post for?
+      quote = response.data;
+      $("#text").css({
+        "text-align": "center",
+        "min-height": "150px",
+        display: "flex",
+        "justify-content": "center",
+        "align-items": "center"
+      });
+      $("#text").html(quote);
     });
-    $("#text").html(quote);
+    });
   });
-});
+
